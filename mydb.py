76e5a3f24656d58f6b4ee5d67497877c347e8ecb
ChrisRaynor <chen_jim2000@outlook.com>
2022-06-13 22:01:53 +0800

    开始项目 pyqt5 + pycharm + sqlite

---
+++ mydb.py
@@ -0,0 +1,24 @@
+# ./mydb.py
+# 进行对数据库的基本操作
+
+from settings import *
+import sqlite3
+import numpy as np
+import io
+# 向sqlite3注册ndarray
+def adapt_array(arr):
+    out = io.BytesIO()
+    np.save(out, arr)
+    out.seek(0)
+    return sqlite3.Binary(out.read())
+
+def convert_array(text):
+    out = io.BytesIO(text)
+    out.seek(0)
+    return np.load(out)
+# Converts np.array to TEXT when inserting
+sqlite3.register_adapter(np.ndarray, adapt_array)
+
+# Converts TEXT to np.array when selecting
+sqlite3.register_converter("array", convert_array)
+
---
+++ settings.py
@@ -0,0 +1,4 @@
+# ./settings.py
+# 配置文件，包含全局设置
+
+DB_ADDRESS = './database.sqlite'
